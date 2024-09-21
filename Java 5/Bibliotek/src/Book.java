f07aed3ed075891f3af06728896d5af93cdf3dd3
DJmoos3 <isaac.strandh@gmail.com>
2024-09-21 17:21:12 +0200

    Added the library assignment

---
+++ Java 5/Bibliotek/src/Book.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class Book
+{
+    String title;
+    String author;
+
+    int publishingYear;
+    int edition;
+
+    private boolean available;
+
+    ArrayList<String> reserveQueue;
+
+    public Book(String title, String author, int publishingYear, int edition)
+    {
+        this.title = title;
+        this.author = author;
+        this.publishingYear = publishingYear;
+        this.edition = edition;
+        this.available = true;
+        this.reserveQueue = new ArrayList<>();
+    }
+
+    public boolean isAvailable()
+    {
+        return this.available;
+    }
+
+    public boolean loanBook()
+    {
+        if(available)
+        {
+            if (!this.reserveQueue.isEmpty())
+            {
+                this.reserveQueue.removeFirst();
+            }
+            this.available = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public boolean returnBook()
+    {
+        if(!available)
+        {
+            this.available = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return  "Title: " + this.title +
+                " Author: " + this.author +
+                " Publishing year: " + this.publishingYear +
+                " Edition: " + this.edition;
+    }
+    public void reserve(String name)
+    {
+        this.reserveQueue.add(name);
+        for (String s : this.reserveQueue) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ Java 5/Bibliotek/src/Library.java
@@ -0,0 +1,187 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Library
+{
+    ArrayList<Book> books = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    public void run()
+    {
+        String answer;
+        String menu[] = {"Welcome to the library. What can i do for you today? choose with numbers.",
+                "1. Add a book to the library.", "2. Search for a book by name" , "3. Collection",
+                "4. Return a book", "5. Quit"};
+
+        boolean running = true;
+
+
+
+
+        while(running)
+        {
+            for (String s : menu) {
+                System.out.println(s);
+            }
+
+            if(scanner.hasNextInt())
+            {
+                answer = scanner.nextLine();
+                if (answer.equals("1"))
+                {
+                    this.addBook();
+                }
+                else if (answer.equals("2"))
+                {
+                    this.search();
+                }
+                else if(answer.equals("3"))
+                {
+                    this.listOfBooks();
+                }
+                else if(answer.equals("4"))
+                {
+                    this.returnBook();
+                }
+                else if(answer.equals("5"))
+                {
+                    running = false;
+                }
+                else
+                {
+                    System.out.println("Wrong input. Try again.");
+                }
+            }
+            else
+            {
+                System.out.println("Wrong input. Try again.");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private void addBook()
+    {
+        String title;
+        String author;
+
+        int publishingYear;
+        int edition;
+
+
+        System.out.println("What book do you want to add?");
+        System.out.println("Start with title:");
+        title = scanner.nextLine();
+
+        System.out.println("Author:");
+        author = scanner.nextLine();
+
+        System.out.println("Year it was published:");
+        publishingYear = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What edition:");
+        edition = scanner.nextInt();
+        scanner.nextLine();
+
+        books.add(new Book(title, author, publishingYear, edition));
+        System.out.println("Book has been added.");
+    }
+    private void search ()
+    {
+        String title;
+        String answer;
+        System.out.println("What book are you looking for?");
+        title = scanner.nextLine();
+        boolean running;
+        for (Book book : books) {
+            if (book.title.equalsIgnoreCase(title))
+            {
+                System.out.println("We've got that book");
+                System.out.println("Do you wanna borrow said book?");
+                running = true;
+                while (running)
+                {
+                    answer = scanner.nextLine();
+                    if (answer.equalsIgnoreCase("yes") && book.isAvailable() && !book.reserveQueue.isEmpty())
+                    {
+                        System.out.println("What is your name?");
+                        String name = scanner.nextLine();
+                        if(book.reserveQueue.getFirst().equalsIgnoreCase(name))
+                        {
+                            System.out.println("Book borrowed");
+                            book.loanBook();
+                            running = false;
+                        }
+                        else
+                        {
+                            System.out.println("The book is reserved by someone else");
+                            running = false;
+                        }
+                    }
+                    else if (answer.equalsIgnoreCase("yes") && book.isAvailable() && book.reserveQueue.isEmpty())
+                    {
+                        System.out.println("Book has been borrowed");
+                        book.loanBook();
+                        running = false;
+                    }
+                    else if (answer.equalsIgnoreCase("yes") && !book.isAvailable())
+                    {
+
+                        System.out.println("Book isn't available as of now. \ndo you want to reserve it?");
+                        answer = scanner.nextLine();
+                        if (answer.equalsIgnoreCase("yes"))
+                        {
+                            System.out.println("Then type your name to reserve it.");
+                            String name = scanner.nextLine();
+                            book.reserve(name);
+                            System.out.println("Book has been reserved.");
+                        }
+                        else
+                        {
+                            System.out.println("You're welcome to return when it's available");
+                        }
+                        running = false;
+                    }
+                    else if (answer.equalsIgnoreCase("no"))
+                    {
+                        System.out.println("Very well");
+                        running = false;
+                    }
+                    else
+                    {
+                        System.out.println("Wrong input. Input either type: No/Yes ");
+                    }
+                }
+            }
+            else
+            {
+                System.out.println("We do not have that book");
+            }
+        }
+    }
+    public void listOfBooks()
+    {
+        for (Book book : books)
+        {
+           System.out.println(book.toString());
+        }
+    }
+    public void returnBook()
+    {
+        String title;
+        System.out.println("What book do you wanna return?");
+        title = scanner.nextLine();
+        for (Book book : books)
+        {
+            if (book.title.equalsIgnoreCase(title))
+            {
+                book.returnBook();
+                System.out.println("Thanks for returning the book.");
+            }
+            else
+            {
+                System.out.println("That's not on of our books");
+            }
+        }
+    }
+}
